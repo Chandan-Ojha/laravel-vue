f8a742aa8cba5d74f5710b654a84e268ac14128a
Chandan Ojha <chandanojha776@gmail.com>
2023-04-04 20:54:29 +0600

    `updated`

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+
 export default defineConfig({
     plugins: [
         vue(),
@@ -11,6 +12,14 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        })
     ],
     resolve: {
         alias: {
